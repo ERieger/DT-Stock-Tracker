54c8dfe7b96148449b09f258738819b8b99b0d9b
ERieger <e.rieger04@gmail.com>
2022-06-28 12:30:43 +0930

    remove JWT decode

--- templates/login.js
+++ templates/login.js
@@ -1,13 +1,3 @@
-// This seems useful https://developers.google.com/identity/gsi/web/guides/handle-credential-responses-js-functions
-// Need to decode the JWT token from google to access all the properties
 function loginCallback(response) {
-    const responsePayload = decodeJwtResponse(response.credential);
-
-    console.log("ID: " + responsePayload.sub);
-    console.log('Full Name: ' + responsePayload.name);
-    console.log('Given Name: ' + responsePayload.given_name);
-    console.log('Family Name: ' + responsePayload.family_name);
-    console.log("Image URL: " + responsePayload.picture);
-    console.log("Email: " + responsePayload.email);
-
+    console.log(response.credential);
 }
