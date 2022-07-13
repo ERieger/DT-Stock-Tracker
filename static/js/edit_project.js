f082fe748d50a87b653675226e37bb412e26d4d0
ERieger <e.rieger04@gmail.com>
2022-07-13 18:55:46 +0930

    *now with comments!*

--- static/js/edit_project.js
+++ static/js/edit_project.js
@@ -1,4 +1,5 @@
-function edit_project(project) {
+function edit_project(project) { 
+    // Parse the project to the form page using local storage
     localStorage.setItem('project', JSON.stringify(project));
     window.location.href='form';
 }
