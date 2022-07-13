2d61b923a9f6f68cada8d00a8ab8d43de9c81e23
ERieger <e.rieger04@gmail.com>
2022-07-13 18:55:33 +0930

    *now with comments!*

--- static/js/add_project.js
+++ static/js/add_project.js
@@ -1,7 +1,10 @@
-let materials;
+let materials; // Global materials variable
 
+// Define function to update the disabled fields
 $.fn.update_disabled = function () {
+    // Reference to parent card
     let parents = $(this).parentsUntil('div.card-body');
+    // Define refrences to each field
     let inputs = {
         width: $(parents[2]).find('input#width'),
         height: $(parents[2]).find('input#height'),
@@ -9,28 +12,33 @@ $.fn.update_disabled = function () {
         selects: $(parents[2]).find('select#materialSelect')
     }
 
+    // Loop throuhg all inputs in parent card
     $(parents).find('input').each(function () {
-        $(this).prop('disabled', false);
+        $(this).prop('disabled', false); // Set disabled to false
     });
 
+    // Loop through all materials
     for (let i = 0; i < materials.length; i++) {
+        // Get currently selected material
         if (materials[i].id == $(inputs.selects).val()) {
+            // Get type of selected material (1: Board, 2: Plank, 3: Dowel)
             switch (materials[i].type) {
                 case 1: // Board
-                    $(inputs.height).prop('disabled', true);
-                    $(inputs.height).val(undefined);
+                    $(inputs.height).prop('disabled', true); // Disable input
+                    $(inputs.height).val(undefined); // Rest value
                     console.log('disable t');
                     break;
 
                 case 2: // Plank
-                    $(inputs.width).prop('disabled', true);
-                    $(inputs.width).val(undefined);
-                    $(inputs.height).prop('disabled', true);
-                    $(inputs.height).val(undefined);
+                    $(inputs.width).prop('disabled', true); // Disable input
+                    $(inputs.width).val(undefined); // Rest value
+                    $(inputs.height).prop('disabled', true); // Disable input
+                    $(inputs.height).val(undefined); // Rest Value
                     console.log('disable wt');
                     break;
 
                 case 3: // Dowel
+                    // As above...
                     $(inputs.width).prop('disabled', true);
                     $(inputs.width).val(undefined);
                     $(inputs.height).prop('disabled', true);
@@ -39,13 +47,16 @@ $.fn.update_disabled = function () {
                     break;
             }
         } else {
-            continue;
+            continue; // Skip curret loop iteration if not the material
         }
     }
 }
 
-$.fn.newMaterial = function (piece) {
-    if (piece != undefined || piece != null) { // Rendering a piece
+// Define function to add a new material to the project
+$.fn.newMaterial = function (piece) { // Accepts object piece as argument
+    if (piece != undefined || piece != null) { // Piece defined - Rendering a piece
+        // Append the new material row to the parent
+        // Subsitutes the correct piece values in
         $(this).append(`
         <div class="row py-3" id="material-card">
             <div class="card">
@@ -144,8 +155,13 @@ $.fn.newMaterial = function (piece) {
             </div>
         </div>`);
 
+        // Populate the dropdown
+        // (includes the piece object and reference to the row as 'this')
         populate_materials(piece, this);
     } else { // Rendering a blank piece
+        // Render a blank row
+        // populate_materials() populate dropdown
+        // Blank this time as it isn't already defined
         $(this).append(`
         <div class="row py-3" id="material-card">
             <div class="card">
@@ -250,42 +266,54 @@ $.fn.newMaterial = function (piece) {
         </div>`);
     }
 
-
-    $('select').off('change');
-    $('select').on('change', function () { $(this).update_disabled() });
+    $('select').off('change'); // Remove existing event listener
+    $('select').on('change', function () { $(this).update_disabled() }); // Bind new one with refernce to dropdown
+    // Find last dropdown row
+    // Update the disabled fields with reference to the dropdown
     $(this).find('select#materialSelect').last().each(function () { $($(this)).update_disabled(); });
 };
 
 function populate_materials(piece, elem) {
-    let options = '';
-
-    if (piece != undefined || piece != null) {
+    // Piece and elem are used to target and fill a defined material
+    // This is used when editing a project
+    if (piece != undefined || piece != null) { // If rendering a defined material
+        // Find last dropdown
         $(elem).find('select#materialSelect').last().each(function () {
+            // Loop through materials
             for (let i = 0; i < materials.length; i++) {
+                // Append each option to the target element
                 $(this).append(`<option value="${materials[i].id}">${materials[i].name}</option>`);
             }
         });
+        
+        // Set the value of the dropdown
         $(elem).find('select#materialSelect').last().val(piece.material)
-        // console.log($(elem).find('select#materialSelect').last().val());
-    } else {
-        for (let i = 0; i < materials.length; i++) {
+    } else { // Blank material
+        let options = ''; // String to return
+        for (let i = 0; i < materials.length; i++) { // Loop through material
+            // Append the option to the return string
             options += `<option value="${materials[i].id}">${materials[i].name}</option>`;
         }
 
-        return options;
+        return options; // Return the string
     }
 
 }
 
+// Function to delete the material row
+// Accepts reference to the row as an argument
+// Finds the parent container
+// Checks if there is more than one material
+// > 1 - removes the row
+// < 1 prevents the row being removed
 function delete_material(elem) { $(elem).parents('div.material-container').find('div#material-card').length > 1 ? $(elem).parents('div#material-card').remove() : console.log('How about no...') };
 
-window.onload = () => {
+window.onload = () => { // On window load
     $.ajax({
         async: false, //We do not want this to be asynchonous, otherwise data will not be returned
         url: "/material/list",
         success: function (response) {
-            console.log(response);
-            materials = response;
+            materials = response; // Set global materials variable (because I couldn't be bothered parseing it aroud)
         },
         error: function (error) {
             //Oh no! There was an error, print it.
@@ -293,51 +321,55 @@ window.onload = () => {
         }
     });
 
-    let editProject = JSON.parse(localStorage.getItem('project'));
+    let editProject = JSON.parse(localStorage.getItem('project')); // Get the project from local storate
 
     if (editProject == null || editProject == undefined) { // Not editing an existing project
         $('.material-container').newMaterial() // Add first material box
         $('#save-btn').click(() => { add_project() }); // Add an action to the save button
     } else { // Editing an existing project
+        // Set various fields to defined 
         $('#projectName').val(editProject.name);
         $('#projectClass').val(editProject.class);
         $('#projectDesc').val(editProject.desc);
         
+        // Loop through materials and spawn a new row parseing the material
         editProject.pieces.forEach(function (piece) {
             $('.material-container').newMaterial(piece);
         });
 
+        // Save ID to session storage for when you submit the edit
         sessionStorage.setItem('_id', editProject["_id"]["$oid"]);
         
         $('#save-btn').click(() => { edit_project() }); // Add an action to the save button
-        localStorage.removeItem('project');
+        localStorage.removeItem('project'); // Remove object from local storage
     }
 };
 
 function add_project() {
-    let project = {
+    let project = { // Define project JSON
         _id: undefined,
-        name: $('#projectName').val(),
-        class: $('#projectClass').val(),
-        desc: $('#projectDesc').val(),
-        thumb: 'https://i.quotev.com/img/q/u/18/10/21/g7fp3vu7ce.jpg',
-        pieces: [],
-        complete: false
+        name: $('#projectName').val(), // Get name
+        class: $('#projectClass').val(), // Get class
+        desc: $('#projectDesc').val(), // Get description
+        thumb: 'https://i.quotev.com/img/q/u/18/10/21/g7fp3vu7ce.jpg', // Set image URL
+        pieces: [], // Define pieces
+        complete: false // Not complete
     }
 
+    // Loop through all material rows
     $('.material-container div.card').each(function () {
-        let material = $(this).find('#materialSelect').val();
+        let material = $(this).find('#materialSelect').val(); // Get selected material
 
         let piece = {
-            material: material,
-            type: materials.find(x => x.id === material).type,
-            w: Number($(this).find('#width').val()),
-            l: Number($(this).find('#length').val()),
-            h: Number($(this).find('#height').val()),
-            qty: Number($(this).find('#qty').val())
+            material: material, // Material
+            type: materials.find(x => x.id === material).type, // Who even knows at this point
+            w: Number($(this).find('#width').val()), // Width
+            l: Number($(this).find('#length').val()), // Lenght
+            h: Number($(this).find('#height').val()), // Height
+            qty: Number($(this).find('#qty').val()) // Quantity
         }
 
-        project.pieces.push(piece);
+        project.pieces.push(piece); // Push object to pieces array
     });
 
     $.ajax({
